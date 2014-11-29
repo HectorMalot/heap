93b7646424425b5f11e254025417aaa1b48692ac
Dennis de Reus <dennisdereus@gmail.com>
2014-11-29 17:15:31 +0100

    bugfix: Heap is a class, not a module

--- lib/generators/heap/install_generator.rb
+++ lib/generators/heap/install_generator.rb
@@ -1,6 +1,6 @@
 require 'rails/generators/base'
 
-module Heap
+class Heap
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../templates", __FILE__)
