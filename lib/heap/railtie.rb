b9e572c5bd75b94d5b7b6903769f8dbda029ac92
Dennis de Reus <dennisdereus@gmail.com>
2014-11-29 17:15:53 +0100

    Bugfix: Heap is a class, not a module

--- lib/heap/railtie.rb
+++ lib/heap/railtie.rb
@@ -1,6 +1,6 @@
 require 'heap/view_helpers'
 
-module Heap
+class Heap
   class Railtie < Rails::Railtie
     initializer "heap.view_helpers" do
       ActiveSupport.on_load( :action_view ){ include Heap::ViewHelpers }
