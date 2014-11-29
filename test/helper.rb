eb7e5924cef8d09c19b1419d00cb96401acaa0f8
Dennis de Reus <dennisdereus@gmail.com>
2014-11-29 14:18:54 +0100

    Added tests for events and identify actions

--- test/helper.rb
+++ test/helper.rb
@@ -8,13 +8,9 @@ end
 
 SimpleCov.configure do
   clean_filters
-  load_adapter 'test_frameworks'
+  load_profile 'test_frameworks'
 end
 
-ENV["COVERAGE"] && SimpleCov.start do
-  add_filter "/.rvm/"
-end
-require 'rubygems'
 require 'bundler'
 begin
   Bundler.setup(:default, :development)
@@ -23,12 +19,13 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-require 'test/unit'
-require 'shoulda'
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'heap'
+require 'minitest/autorun'
+require 'shoulda'
+require 'webmock/minitest'
 
-class Test::Unit::TestCase
+class MiniTest::Test
 end
+
+MiniTest.autorun
--- test/test_heap.rb
+++ test/test_heap.rb
@@ -1,7 +1,25 @@
 require 'helper'
 
-class TestHeap < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class TestHeap < MiniTest::Test
+  should "post events to https://heapanalytics.com/api/track" do
+    stub_request(:post, "https://heapanalytics.com/api/track").
+      with(:body => {:app_id => '123', :event => 'test', :identity => 'test@example.com', :properties => {:property1 => 'hello', :property2 => 'world'}},
+         :headers => {'Content-Type'=>'application/json'}).
+      to_return(:body => "OK")
+
+    Heap.app_id = 123
+    result = Heap.event('test','test@example.com', :property1 => 'hello', :property2 => 'world')
+    assert_equal result.to_s, "OK"
+  end
+
+  should "post identities to https://heapanalytics.com/api/identify" do
+    stub_request(:post, "https://heapanalytics.com/api/identify").
+      with(:body => {:app_id => '123', :identity => 'test@example.com', :properties => {:property1 => 'hello', :property2 => 'world'}},
+         :headers => {'Content-Type'=>'application/json'}).
+      to_return(:body => "OK")
+
+    Heap.app_id = 123
+    result = Heap.identify('test@example.com', :property1 => 'hello', :property2 => 'world')
+    assert_equal result.to_s, "OK"
   end
 end
