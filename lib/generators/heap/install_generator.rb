294f88b2b3a64148aad08f29fbd1d6ffc429339a
Dennis de Reus <dennisdereus@gmail.com>
2014-11-29 15:32:58 +0100

    added rails generator for installation

---
+++ lib/generators/heap/install_generator.rb
@@ -0,0 +1,12 @@
+require 'rails/generators/base'
+
+module Heap
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+      def copy_initializer
+        template "heap.rb", "config/initializers/heap.rb"
+      end
+    end
+  end
+end
---
+++ lib/generators/templates/heap.rb
@@ -0,0 +1,2 @@
+# change this to your Heap ID, or use a environment variable to set your ID
+Heap.app_id = ENV["HEAP_ID"]
