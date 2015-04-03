3c9eac31e10c7817ce5bcbbdc693b2175610073a
Josh Strater <jstrater@gmail.com>
2015-04-03 16:34:20 -0700

    Add heap_set_event_properties with test

--- test/test_view_helper.rb
+++ test/test_view_helper.rb
@@ -1,5 +1,11 @@
 require 'helper'
 
+class String
+  def html_safe
+    self
+  end
+end
+
 class HeapHelperTest < MiniTest::Test
   include Heap::ViewHelpers
 
@@ -7,4 +13,21 @@ class HeapHelperTest < MiniTest::Test
     # handle = '123'
     # assert_equal heap_identify(handle), "heap.identify({email: \"#{handle}\" });"
   # end
+  def javascript_tag(options = {})
+    yield
+  end
+
+  def raw(js)
+    js
+  end
+
+  should "set event properties from arbitrary key-value pairs" do
+    js = heap_set_event_properties({
+      :first => "first value",
+      "second" => 2,
+      :third => true
+    })
+
+    assert_equal js, %Q/heap.setEventProperties({"first":"first value","second":2,"third":true});/
+  end
 end
