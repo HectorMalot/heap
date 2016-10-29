90abf31681c56b31fcdb2836183ac74ff187aadb
Alan Hsu <admin@mycallhero.com>
2016-10-28 17:49:39 -0700

    Load heap on production only only by default

--- lib/heap/view_helpers.rb
+++ lib/heap/view_helpers.rb
@@ -3,6 +3,7 @@ require 'json'
 class Heap
   module ViewHelpers
     def heap_analytics(properties = {})
+      return '' unless Rails.env.production?
       config = properties.map {|k,v| "#{k}: #{v}"}.join(", ") if properties.is_a? Hash
       heap_js_block %Q{
         window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
